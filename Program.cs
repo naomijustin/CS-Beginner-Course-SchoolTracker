32f87e0dd6b0761696728021a451705c2b508e22
Naomi Justin <naomi.z.justin@gmail.com>
2022-07-04 09:29:07 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SchoolTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentGrades = new int[] { 80, 77, 45, 87, 53, 43, 67, 56, 89, 99 };
+
+            Console.WriteLine(studentGrades[4]);
+        }
+    }
+}
