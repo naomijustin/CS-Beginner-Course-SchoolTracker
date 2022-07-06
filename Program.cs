a31e54a62e7601614ce64d614a52dadce5b50ede
Naomi Justin <naomi.z.justin@gmail.com>
2022-07-07 08:44:10 +1000

    Added additional value to array to test non-length specified array is still working

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace SchoolTracker
     {
         static void Main(string[] args)
         {
-            var studentGrades = new int[] { 80, 77, 45, 87, 53, 43, 67, 56, 89, 99 };
+            var studentGrades = new int[] { 80, 77, 45, 87, 53, 43, 67, 56, 89, 99, 40 };
 
             Console.WriteLine(studentGrades[4]);
         }
